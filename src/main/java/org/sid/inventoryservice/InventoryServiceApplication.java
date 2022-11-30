81eec3de6776d6e2aa34a8763ab3acd53864a3fe
ilyasseelmoulaoui <ilyasseelmoulaoui00@gmail.com>
2022-11-30 10:08:14 +0100

    EoF part2

--- src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -24,9 +25,10 @@ public class InventoryServiceApplication {
 	}
 
 	@Bean
-	CommandLineRunner start(ProductRepository productRepository){
+	CommandLineRunner start(ProductRepository productRepository, RepositoryRestConfiguration restConfiguration){
+		restConfiguration.exposeIdsFor(Product.class);
 		return args -> {
-			productRepository.save(new Product(null, "Ordinateur", 832,15));
+			productRepository.save(new Product(null, "Ordinateur", 832,22));
 			productRepository.save(new Product(null, "Telephone", 425,24));
 			productRepository.save(new Product(null, "Ecouteurs", 40,52));
 			productRepository.findAll().forEach(p -> {
